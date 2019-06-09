ec762b2a91cc1febdfb6bd83655cde1dd8d33b23
stefantrinh1 <stefantrinh1.work@gmail.com>
2019-06-09 23:45:53 +0100

    mobile fixes again reponsive view height added to aap.js script using window height rather than screen height

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.scss';
 import AppRouter from "./router/AppRouter";
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 class App extends React.Component {
 
   render() {
