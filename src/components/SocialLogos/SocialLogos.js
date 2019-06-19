5cb5dba8aa2ac44a6a43b4cb4b4dd527220316ad
stefantrinh1 <stefantrinh1.work@gmail.com>
2019-06-19 23:35:35 +0100

    added labels for accessbility

--- src/components/SocialLogos/SocialLogos.js
+++ src/components/SocialLogos/SocialLogos.js
@@ -7,15 +7,15 @@ const SocialLogos = () => {
     return (
         <div className="sociallogos">
 
-            <a href="https://github.com/stefantrinh1" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/stefantrinh1" target="_blank" aria-label="See Github Repository and Portfolio" rel="noopener noreferrer">
                 <GithubLogo />
             </a>
 
-            <a href="https://www.linkedin.com/in/stefantrinh1/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/stefantrinh1/" target="_blank" aria-label="See Linkedin Profile and Employment History" rel="noopener noreferrer">
                 <LinkedinLogo />
             </a>
 
-            <a href="https://www.instagram.com/travel.code.geek/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/travel.code.geek/" target="_blank" aria-label="See Instagram for Social" rel="noopener noreferrer">
                 <InstagramLogo />
             </a>
             
