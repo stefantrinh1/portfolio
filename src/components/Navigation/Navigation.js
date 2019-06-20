c22180d4eff031a8fe606d4e5ab894d0bfa7b2ff
stefantrinh1 <stefantrinh1.work@gmail.com>
2019-06-20 23:28:05 +0100

    navigation testing and fixing.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -10,98 +10,69 @@ class Navigation extends React.Component {
         super();
         this.navElement = document.querySelector(".nav");
         this.state = {
-            navigationOpen: false,
+            navigationOpen: null,
         }
     }
 
     componentDidMount() {
-        // console.log(this.navElement());
-        
+        this.DetermineNavState()
+
+        // event listener for when screen changes size
+        window.addEventListener('resize', () => { this.DetermineNavState() });
     }
-    // returns Nav Width
-    NavWidth() {
-        
-        const navElement = document.querySelector(".nav");
-        const style = getComputedStyle(navElement);
-        const navWidth = style.getPropertyValue('width');
-        return navWidth
+
+    // function to determine whether to show nav based on desktop show nav or hide on mobile.
+    DetermineNavState() {
+        window.innerWidth < 768 ? this.setState({ navigationOpen: false }) : this.setState({ navigationOpen: true })
     }
-    
+
     // Handles whether to open or close depending on if nav is open in state
     ShowOrHideNav() {
         return (!this.state.navigationOpen ? this.OpenNav() : this.CloseNav())
     }
 
-
     // brings Nav Onto Screen
-    OpenNav() {
-        console.log("Navigation Menu Opened");
-        const navElement = document.querySelector(".nav");
-        navElement.style.left = "0%";
-        this.setState({
-            navigationOpen: true
-        })
-       
-        // when the nav opens event listener is added so the menu 
-        // can be closed when clicking a nav or outside of nav menu
-        this.HandleNavAction(navElement)
-     
-    }
+    OpenNav() { this.setState({ navigationOpen: true }) }
 
 
     // move the navigation off screen
-    CloseNav() {
-        console.log("Navigation Menu Closed");
-        const navElement = document.querySelector(".nav");
-        const navWidth = this.NavWidth(); 
+    CloseNav() { this.setState({ navigationOpen: false }) }
 
-        // this if statement makes sure that only the nav on mobile closes and does not do the same on desktop.
-        if (window.innerWidth < 768) {
-            // grabs the width of the nav from the dom in Pixels
-            navElement.style.left = `-${navWidth}`
-            this.setState({
-                navigationOpen: false
-            })
+    // // Handles the Clicks
+    // HandleNavAction(event) {
 
-        }
+    //     const navElement = document.querySelector(".nav");
+    //     // const navLink = document.querySelectorAll(".nav__nav-item-link");
 
-    }
+    //     const outsideClickListener = (event) => {
+    //         if (!navElement.contains(event.target) || event.target === navLink) {
+    //             this.CloseNav();
+    //             removeClickListener();
+    //         }
+    //     }
 
-    // Handles the Clicks
-    HandleNavAction(event) {
-
-        const navElement = document.querySelector(".nav");
-        const navLink = document.querySelectorAll(".nav__nav-item-link");
-    
-        const outsideClickListener = (event) => {
-            if(!navElement.contains(event.target) || event.target === navLink ) {
-                this.CloseNav();
-                removeClickListener();
-              }
-        }
-
-        const removeClickListener = () => {
-            document.removeEventListener('click', outsideClickListener)
-            document.removeEventListener("click", navClickListener)
-        }
+    //     const removeClickListener = () => {
+    //         document.removeEventListener('click', outsideClickListener)
+    //         document.removeEventListener("click", navClickListener)
+    //     }
 
 
-        const navClickListener = (event) => {
-            
-            navLink.forEach(element => {
-                if (element.contains(event.target)) {
-                    // console.log("navclicked")
-                    this.CloseNav();
-                    removeClickListener();
-                }
-            });
-        }
+    //     const navClickListener = (event) => {
 
-        document.addEventListener('click', outsideClickListener)
-        document.addEventListener("click", navClickListener)
+    //         navLink.forEach(element => {
+    //             if (element.contains(event.target)) {
+    //                 // console.log("navclicked")
+    //                 this.CloseNav();
+    //                 removeClickListener();
+    //             }
+    //         });
+    //     }
 
-    }
-    
+    //     document.addEventListener('click', outsideClickListener)
+    //     document.addEventListener("click", navClickListener)
+
+    // }
+
 
 
     render() {
@@ -126,6 +97,20 @@ class Navigation extends React.Component {
             );
         });
 
+        let navMenu = (
+            <div className="nav">
+                <div className="nav__nav-icon">
+                    <NavLink to="/">
+                        <BrandLogo />
+                    </NavLink>
+                </div>
+                <ul className="nav__nav-menu">
+                    {navItems}
+                </ul>
+                <div className="nav__opaque-block" />
+            </div>
+        )
+
 
         return (
 
@@ -134,23 +119,8 @@ class Navigation extends React.Component {
                     <Hamburger />
                 </div>
 
-                <div className="nav">
-                    <div className="nav__nav-icon">
-                        <NavLink to="/">
-                            <BrandLogo />
-                        </NavLink>
-                    </div>
-                    <ul className="nav__nav-menu">
-                        {navItems}
-                    </ul>
-                </div>
+                {this.state.navigationOpen ? navMenu : null}
 
-                {this.state.navigationOpen ? 
-                <div className="nav__opaque-block">
-
-                </div>: null
-
-}
             </div>
         )
     }
