869d1a0db95f58f0c594d6c642ae9b4174d031f4
stefantrinh1 <stefantrinh1.work@gmail.com>
2019-06-23 13:04:53 +0100

    added logos and banners

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,43 @@
 import React from "react";
+import * as contentful from "contentful";
 
 class Portfolio extends React.Component {
+
+
+        state = {
+            isPortfolioLoading: true
+        }
+
+        // Contentful's Client 
+        client = contentful.createClient({
+            space: '6uk9nhmjdkre',
+            accessToken: 'vRPrbrCwApcb4AXyT2yS3mXp2JNvSMdzTZ1k2jhmEAA'
+        })
+
+        // ====  Queries  ====
+        PortfolioQuery = {
+            content_type: "portfolio",
+        }
+
+        // This is a Generic Fetch By ContentType Function for Contentful. It takes a query 
+        FetchByContentType = (query) => this.client.getEntries(query)
+
+        SetPortfolioContent = response => {
+            this.setState({
+                isPortfolioLoading: false,
+                // the array is brought in ascending order
+                "careerJSON": response.items
+            })
+            console.log(response)
+        }
+
+        componentDidMount() {
+            // Functions to Fetch Data from Contentful
+            this.FetchByContentType(this.PortfolioQuery).then(this.SetPortfolioContent).catch(console.error)
+        }
+
+
+
 
     render() {
         return (
