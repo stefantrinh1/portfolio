37d8495c40499237ea48ec850143c47901b0aaae
stefantrinh1 <stefantrinh1.work@gmail.com>
2019-06-29 16:23:40 +0100

    attempting mobile fix for iphone broswer

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,6 +3,18 @@ import * as contentful from "contentful";
 import LoadingPage from "../LoadingPage/LoadingPage"
 import Project from "./Project"
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// event listener for when screen changes size
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 class Portfolio extends React.Component {
 
     state = {
