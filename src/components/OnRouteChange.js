3d753b28f8c1db97345c44c78c99afc1351d28dd
stefantrinh1 <stefantrinh1.work@gmail.com>
2020-02-09 10:16:57 -0700

    removed unsed imports and tidieda couple of lines

--- src/components/OnRouteChange.js
+++ src/components/OnRouteChange.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { useEffect } from "react";
 import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import { toggleNav } from "../redux/actions/Navigation";
+
 import disableScroll from 'disable-scroll'
+
+// routes are wrapped in this HOC
 
 const OnRouteChange = props => {
   useEffect(() => {
+    // listener for when route changes
     const unlisten = props.history.listen((location, action) => {
       const navMenu = document.getElementById("navmenucontainer");
-      navMenu.style.left = "-100%";
-      disableScroll.off()
+      navMenu.style.left = "-100%"; // closes the nav when the route changes
+      disableScroll.off() // turn off scroll when route changes
     });
   });
 
