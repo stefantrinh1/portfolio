64663b59fac6f58764a0d99c1eef18afbb249282
stefantrinh1 <stefantrinh1.work@gmail.com>
2020-02-09 10:19:27 -0700

    added redux store provider to wrap app in. and also added scrollup to top button to all pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import AppRouter from "./router/AppRouter";
+import ScrollUpButton from "react-scroll-up-button";
+import Store from "./store/configureStore";
+import { createStore } from "redux";
+import { Provider } from "react-redux"
 import "./App.scss";
 
+const store = createStore(Store)
+
 const App = () => {
-    return (
-      <div className="App">
-       
-          <AppRouter />
-        
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <AppRouter />
+        <ScrollUpButton />
+      </Provider>
+    </div>
+  );
+};
 
 export default App;
