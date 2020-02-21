1d781d945717cb17226a2188850d59baaf64b94c
stefantrinh1 <stefantrinh1.work@gmail.com>
2020-02-20 22:10:34 -0700

    added action that fetches stack names from contentful

--- src/actions/PortfolioFilter.js
+++ src/actions/PortfolioFilter.js
@@ -1,3 +1,5 @@
+import * as contentful from "contentful";
+
 // Navigation Actions
  export const setTextFilter = (text) => ({
     type: "SET_TEXT_FILTER",
@@ -13,3 +15,32 @@
     type: "SET_STACKS",
     stackList: stackList // takes a true or false
   })
+
+  export const fetchStackOptions = () => dispatch => {
+    const client = contentful.createClient({
+      space: "6uk9nhmjdkre",
+      accessToken: "vRPrbrCwApcb4AXyT2yS3mXp2JNvSMdzTZ1k2jhmEAA"
+    });
+ 
+    const FetchByContentType = query => client.getEntries(query);
+
+    FetchByContentType({
+      content_type: "about"
+    }).then(data => {
+      const stacks = data.items[0].fields.techStack.map((stack)=> {
+        return stack.fields.name
+      })
+      dispatch({
+        type: "FETCH_STACK_OPTIONS",
+        stackOptions:stacks,
+        loading:false,
+      });
+    }).catch((error)=> {
+      dispatch({
+        type: "FETCH_STACK_OPTIONS",
+        stackOptions:[],
+        loading:false,
+        error:error
+      })
+    });
+  }
--- src/actions/PortfolioProjects.js
+++ src/actions/PortfolioProjects.js
@@ -1,7 +1,6 @@
 import * as contentful from "contentful";
 
 export const startSetProjects = () => dispatch => {
-  console.log("function hit");
   const client = contentful.createClient({
     space: "6uk9nhmjdkre",
     accessToken: "vRPrbrCwApcb4AXyT2yS3mXp2JNvSMdzTZ1k2jhmEAA"
@@ -12,14 +11,12 @@ export const startSetProjects = () => dispatch => {
   };
   const FetchByContentType = query => client.getEntries(query);
   FetchByContentType(PortfolioQuery).then(data => {
-    console.log(data);
     dispatch({
       type: "SET_PROJECTS",
       projects: data.items,
       loading:false,
     });
   }).catch((error)=> {
-    console.log(error)
     dispatch({
       type: "SET_PROJECTS",
       projects: [],
