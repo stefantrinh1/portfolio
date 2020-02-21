bfa5ac9703e5ed65fcf15d25578c349fcc78864f
stefantrinh1 <stefantrinh1.work@gmail.com>
2020-02-20 22:07:34 -0700

    adding projects not found Ui component for when filters does match project

---
+++ src/components/Error/InvalidCriteria.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SadFace from "../../icons-logos/icons/SadFace"
+
+export default () => (
+    <div style={{margin: '0 auto',}}>
+    <div style={{ display: 'flex',justifyContent: 'center', marginBottom: "1em"}}>
+      <SadFace />
+    </div>
+    <p style={{color: 'yellow',fontWeight: 'bold'}}>I'm Sorry, Nothing Meets Your Criteria</p>
+  </div>
+);
